61cf948a8c46b68e8c586ca2c6f64d4b1f483dce
Sanal Mishra <sanamishra@BLR.ALLEGISINDIA.COM>
2022-04-11 12:59:34 +0530

    JSP

--- Java_FSD_Practice/src/com/JavaLive/Session3/BinaryDecimal.java
+++ Java_FSD_Practice/src/com/JavaLive/Session3/BinaryDecimal.java
@@ -6,19 +6,17 @@ public class BinaryDecimal {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
-		int n = s.length();
 		int num = Integer.parseInt(s);
 		int sum = 0;
-		int i =0;
-			while(num>0) {
-				int temp = num%10;
-				sum += ((temp) * Math.pow(2, i++));
-				num = num/10;
-			}
-			
-		
+		int i = 0;
+		while (num > 0) {
+			int temp = num % 10;
+			sum += ((temp) * Math.pow(2, i++));
+			num = num / 10;
+		}
+
 		System.out.println("The binary of " + s + " is :" + sum);
-		
+
 		sc.close();
 	}
 }
